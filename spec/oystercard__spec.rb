8f951886435cfacc51cecd12f2fd247a3e4bfdc9
EC2 Default User <ec2-user@ip-172-31-28-29.eu-west-2.compute.internal>
2020-06-15 15:13:32 +0000

    end of chapter 4 added balance

--- spec/oystercard__spec.rb
+++ spec/oystercard__spec.rb
@@ -1,2 +1,10 @@
+require 'oystercard'
+
 describe Oystercard do 
+    describe 'initialize' do 
+      it 'initializes balance under oystercard class' do 
+        oystercard = Oystercard.new
+        expect(oystercard.balance).to eq(0)
+       end 
+    end 
 end 
