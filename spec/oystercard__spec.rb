0d5f5ad965429b76fa114c624bcdb334159858fa
Arav <arav.patel@gmail.com>
2020-06-16 14:33:48 +0100

    Completed Chapter 5

--- spec/oystercard__spec.rb
+++ spec/oystercard__spec.rb
@@ -1,10 +1,18 @@
 require 'oystercard'
 
 describe Oystercard do 
-    describe 'initialize' do 
-      it 'initializes balance under oystercard class' do 
-        oystercard = Oystercard.new
-        expect(oystercard.balance).to eq(0)
-       end 
+  let(:oystercard) { Oystercard.new }
+
+  describe '#initialize' do 
+    it 'initializes balance under oystercard class' do 
+      expect(oystercard.balance).to eq(0)
     end 
+  end 
+
+  describe "#top_up" do
+    it "should equal 5 if we #top_up the balance by 5" do
+      oystercard.top_up(5)
+      expect(oystercard.balance).to eql(5)
+    end
+  end
 end 
