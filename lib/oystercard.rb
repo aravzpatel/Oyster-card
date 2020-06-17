9d35ef0f55ce85a35ba10a553cbabb7cabbeabaf
Arav <arav.patel@gmail.com>
2020-06-17 16:00:49 +0100

    Chapter 11 completed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,28 +1,30 @@
 class Oystercard
     MAXIMUMLIMIT = 90
     FARE = 1
-    attr_reader :balance, :location 
+    attr_reader :balance, :location , :entry_station
     
     def initialize
       @balance = 0
-      @location = false
+      # @location = false
     end 
 
     def top_up(value)
       @balance + value > MAXIMUMLIMIT ? (raise "You've reached the maximum limit, which is #{MAXIMUMLIMIT}") : @balance += value
     end
 
-    def touch_in
+    def touch_in(station)
       balance < FARE ? (raise StandardError.new "you have insufficient funds") : in_journey?
+      @entry_station = station
     end
     
     def touch_out
       in_journey?
       deduct
+      @entry_station = nil
     end
     
     def in_journey?
-      @location = !@location
+      @entry_station != nil
     end
 
     private
--- spec/oystercard__spec.rb
+++ spec/oystercard__spec.rb
@@ -2,6 +2,7 @@ require 'oystercard'
 
 describe Oystercard do 
   let(:oystercard) { Oystercard.new }
+  let(:station){ double :station }
 
   describe '#initialize' do 
     it 'initializes balance under oystercard class' do 
@@ -31,30 +32,40 @@ describe Oystercard do
 
   describe "#touch_in" do
     it "should set in_journey? to true" do
-      oystercard.top_up(5)
-      oystercard.touch_in
-      expect(oystercard.location).to eql(true)
+        oystercard.top_up(5)
+        oystercard.touch_in(station)
+        expect(oystercard.in_journey?).to eql(true)
+    end
+    
+    it "remembers the station" do
+        oystercard.top_up(5)
+        oystercard.touch_in(station)
+        expect(oystercard.entry_station).to eql(station)
     end
 
     it "should only let you touch in if you have a minimum amount for a journey" do
-      expect { oystercard.touch_in }.to raise_error(StandardError, "you have insufficient funds")
-    end
+      expect { oystercard.touch_in(station) }.to raise_error(StandardError, "you have insufficient funds")
+    end    
   end
   
   describe "#touch_out" do
     
     before "top up the card" do
       oystercard.top_up(5)
-      oystercard.touch_in
+      oystercard.touch_in(station)
     end
 
     it "should set in_journey? to false" do
-      expect{ oystercard.touch_out }.to change{oystercard.location}.from(true).to(false)
+      expect{ oystercard.touch_out }.to change(oystercard, :in_journey?).from(true).to(false)
     end
 
     it "should reduce the balance by the FARE amount" do
       expect { oystercard.touch_out }.to change{oystercard.balance}.by(-Oystercard::FARE)
     end
-    
+
+    it "station should be nil" do
+      expect {oystercard.touch_out}.to change{oystercard.entry_station}.from(station).to(nil)
+    end
   end
+
 end 
